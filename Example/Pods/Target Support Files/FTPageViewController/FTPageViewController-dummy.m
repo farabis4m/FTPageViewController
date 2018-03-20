f312ff9f714cdeefe87b48b2e49daa05e7cbe9e2
Thahir Maheen <thahir@farabi.ae>
2018-03-20 12:30:16 +0400

    Add pods

---
+++ Example/Pods/Target Support Files/FTPageViewController/FTPageViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FTPageViewController : NSObject
+@end
+@implementation PodsDummy_FTPageViewController
+@end
---
+++ Example/Pods/Target Support Files/Pods-FTPageViewController_Tests/Pods-FTPageViewController_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FTPageViewController_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FTPageViewController_Tests
+@end
